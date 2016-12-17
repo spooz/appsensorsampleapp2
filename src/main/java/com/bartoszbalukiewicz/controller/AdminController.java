a2d2c3ffd8be0955b48038e1bacf79f8c6427b6c
Bartosz Balukiewicz <bartekbalukiewicz@gmail.com>
2016-12-18 00:04:50 +0100

    poprawki ui

--- src/main/java/com/bartoszbalukiewicz/controller/AdminController.java
+++ src/main/java/com/bartoszbalukiewicz/controller/AdminController.java
@@ -1,8 +1,11 @@
 package com.bartoszbalukiewicz.controller;
 
 import com.bartoszbalukiewicz.service.ForumService;
+import com.bartoszbalukiewicz.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -15,6 +18,9 @@ public class AdminController {
     @Autowired
     private ForumService forumService;
 
+    @Autowired
+    private NotificationService notificationService;
+
     @GetMapping("/topic/{id}/delete")
     public String deleteTopic(@PathVariable Long id) {
         forumService.deleteTopic(id);
@@ -22,6 +28,17 @@ public class AdminController {
     }
 
     @GetMapping("/message/{id}/delete")
-    public void deleteMessage(@PathVariable Long id) {forumService.deleteMessage(id);}
+    public void deleteMessage(@PathVariable Long id) {
+        forumService.deleteMessage(id);}
+
+
+    @PreAuthorize("@securityService.isAdmin()")
+    @GetMapping("/admin")
+    public String getAdminPage(Model model) {
+
+        model.addAttribute("notifications", notificationService.findAll());
+
+        return "admin";
+    }
 
 }
--- src/main/java/com/bartoszbalukiewicz/service/NotificationService.java
+++ src/main/java/com/bartoszbalukiewicz/service/NotificationService.java
@@ -1,14 +1,19 @@
 package com.bartoszbalukiewicz.service;
 
 import com.bartoszbalukiewicz.model.Notification;
+import com.bartoszbalukiewicz.model.view.NotificationView;
 import com.bartoszbalukiewicz.repository.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by Bartek on 06.12.2016.
  */
 @Service
+@PreAuthorize("@securityService.isAdmin()")
 public class NotificationService {
 
     @Autowired
@@ -19,5 +24,9 @@ public class NotificationService {
         notification.setSource(source);
         notification.setIpAddress(ipAddress);
         notificationRepository.save(notification);
+    }
+
+    public List<NotificationView> findAll() {
+        return notificationRepository.getAll();
     }
 }
